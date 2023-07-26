964b1a49b465fbe9e119ae2a7923601c84845742
SaiKrishnaPonnmaneni <psai8307@gmail.com>
2023-07-26 11:01:00 +0530

    sms

--- src/main/java/pwd/Test.java
+++ src/main/java/pwd/Test.java
@@ -6,7 +6,11 @@ public class Test {
 
 	public static void main(String[] agrs) {
 		pwd pd=new pwd(123);
+<<<<<<< Updated upstream
      pwd pd=new pwd(125);
+=======
+     pwd pd=new pwd(124);
+>>>>>>> Stashed changes
 	}
 
 }
